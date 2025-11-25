f261033c2b6d2ac7bafc2d6f46cb60011a086025
ragberr <krystian.pawel.robak@gmail.com>
2025-11-25 19:06:33 +0100

    Init

---
+++ Source/GuardGameJam/Public/UGameMessageSubsystem.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/Subsystem.h"
+#include "GameplayTagContainer.h"
+#include "UGameMessageSubsystem.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class GUARDGAMEJAM_API UUGameMessageSubsystem : public USubsystem
+{
+	GENERATED_BODY()
+	
+public:
+
+
+private:
+
+};
